4305a6dd6b23c7543958382e9535162630a7d8d5
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2023-08-31 20:10:11 +0600

    first commit

---
+++ src/components/addCoffee/AddCoffee.jsx
@@ -0,0 +1,18 @@
+import './AddCoffee.css'
+
+const AddCoffee = () => {
+  return (
+    <div>
+      <h2>Add Coffee</h2>
+
+      <form>
+        <div>
+        
+          
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddCoffee;
---
+++ src/components/updateCoffee/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+import './'
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h2>Update a Coffee</h2>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
