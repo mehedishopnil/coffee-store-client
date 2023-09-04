a8a27ea66496e6fe961dffccb88acb33764dfe16
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2023-09-05 01:25:00 +0600

    new update of home page

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,24 +5,18 @@ import CoffeeCard from "./coffeeCard/CoffeeCard";
 const Home = () => {
   const coffees = useLoaderData();
   console.log(coffees.name);
-  
 
   return (
-    <div className="container mx-auto">
-      <h1 className="text-5xl  my-5 text-center text-gray-700">Coffee Store</h1>
-      <h1 className="text-3xl  my-5 text-center text-gray-700">
-        Total Coffees: {coffees.length}
-      </h1>
-      <div className="grid grid-cols-2 gap-5 my-8">
-      {
-        coffees.map(coffee => <CoffeeCard
-        key={coffee._id}
-        coffee = {coffee}
-        ></CoffeeCard>)
-      }
-      </div>
-
-      
+    <div className="home-body container mx-auto md:mx-auto">
+      {/* Popular Coffee Section */}
+      <section className="popular-products">
+        <h1 className="text-5xl  my-5 text-center ">Our Popular Products</h1>
+        <div className="grid grid-cols-2 gap-5 my-8">
+          {coffees.map((coffee) => (
+            <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>
+          ))}
+        </div>
+      </section>
     </div>
   );
 };
